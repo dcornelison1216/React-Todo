35eb5174cc2ac451e866f76db1f44dad8423f59e
derek-cornelison <dcornelison1216@gmail.com>
2020-05-06 19:52:32 -0400

    styling changes and deployed build

--- src/components/AppStyles.js
+++ src/components/AppStyles.js
@@ -8,6 +8,10 @@ export const Form = styled.form`
 display: flex;
 flex-direction: row;
 justify-content: space-between;
+@media (max-width: 500px) {
+  flex-direction: column;
+  text-align: center;
+}
 `;
 
 export const Button = styled.button`
@@ -24,6 +28,17 @@ color: yellow;
 height: 1.8rem;
 border-color: black;
 margin: 1rem 0 0 1rem;
+@media (max-width: 500px) {
+  width: 80%;
+}
+`;
+
+export const TodoListDiv = styled.div`
+@media (max-width: 500px) {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 `;
 
 export const Input = styled.input`
@@ -33,6 +48,9 @@ border-bottom: 2px solid yellow;
 height: 1.5rem;
 margin: 1rem 0 0 1rem;
 color: yellow;
+@media (max-width: 500px) {
+  width: 80%;
+}
 `;
 
 export const AppContainer = styled.div`
@@ -45,12 +63,22 @@ export const ItemDiv = styled.div`
 margin-left: 1rem;
 max-width: 33%;
 min-height: 2rem;
+@media (max-width: 500px) {
+  max-width: 80%;
+  width: 80%;
+  text-align: center;
+  margin: auto;
+  margin: .5rem 0 .5rem 0;
+}
 `;
 
 export const Item = styled.p`
 font-size: 1.5rem;
 margin-left: .5rem;
 text-transform: capitalize;
+@media (max-width: 500px) {
+  align-items: center;
+}
 `;
 
 export const Headline = styled.h2`
@@ -61,4 +89,9 @@ margin: 1rem 0 0 1rem;
 export const AddClearDiv = styled.div`
 display: flex;
 flex-direction: row;
+@media (max-width: 500px) {
+  flex-direction: column;
+  width: 100%;
+  align-items: center;
+}
 `;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import Todo from './Todo';
-import { ClearButton } from './AppStyles';
+import { ClearButton, TodoListDiv } from './AppStyles';
 
 class TodoList extends React.Component {
   render() {
     return (
-      <div className="todo-list">
+      <TodoListDiv>
         {this.props.todolist.map(todo => (
           <Todo toggleTodoCompleted={this.props.toggleTodoCompleted} key={todo.id} todo={todo} />
         ))}
         <ClearButton className="clear-btn" onClick={this.props.clearCompleted}>
           Clear Completed
         </ClearButton>
-      </div>
+      </TodoListDiv>
     );
   };
 };
